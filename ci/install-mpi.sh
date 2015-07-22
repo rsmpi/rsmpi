f0deef12050eee6ceda4faf33e35e4512e591016
Benedikt Steinbusch <ben@rockshrub.de>
2015-07-22 18:45:52 +0200

    Build MPI libraries from source for CI.

---
+++ ci/install-mpi.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+set -e
+
+DOWNLOAD_DIR="${HOME}/tmp"
+INSTALL_PREFIX="${HOME}/opt"
+
+PACKAGE_NAME="${MPI_LIBRARY:?"MPI_LIBRARY not set!"}-${MPI_LIBRARY_VERSION:?"MPI_LIBRARY_VERSION not set!"}"
+INSTALL_PREFIX="${INSTALL_PREFIX}/${PACKAGE_NAME}"
+TARBALL_NAME="${PACKAGE_NAME}.tar.gz"
+
+if [[ -d "${INSTALL_PREFIX}" ]]
+then
+  echo "MPI library already installed: ${PACKAGE_NAME}"
+  exit 0
+fi
+
+case "$MPI_LIBRARY" in
+  openmpi)
+    OPENMPI_SHORTVERSION=$(expr "${MPI_LIBRARY_VERSION}" ":" "\(.\..\)")
+    SOURCE_URL="http://www.open-mpi.org/software/ompi/v${OPENMPI_SHORTVERSION}/downloads/${TARBALL_NAME}"
+    ;;
+  mpich)
+    SOURCE_URL="http://www.mpich.org/static/downloads/${MPI_LIBRARY_VERSION}/${TARBALL_NAME}"
+    ;;
+esac
+
+echo "Installing MPI library: ${PACKAGE_NAME}"
+echo "into: ${INSTALL_PREFIX}"
+echo "Download URL: ${SOURCE_URL}"
+echo "Tarball name: ${TARBALL_NAME}"
+
+mkdir -p "${DOWNLOAD_DIR}"
+cd "${DOWNLOAD_DIR}"
+wget -O "${TARBALL_NAME}" "${SOURCE_URL}"
+tar -xzf "${TARBALL_NAME}"
+
+cd "${PACKAGE_NAME}"
+mkdir -p "${INSTALL_PREFIX}"
+./configure --prefix="${INSTALL_PREFIX}"
+make -j 2
+make -j 2 install
