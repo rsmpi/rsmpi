e1d955e95c5e8f27177f18fe1a799b9496914f9b
Benedikt Steinbusch <ben@rockshrub.de>
2015-07-24 01:18:36 +0200

    Push harder.

--- ci/deploy-docs.sh
+++ ci/deploy-docs.sh
@@ -7,5 +7,4 @@ travis-cargo doc
 # Manual doc deployment
 echo "<meta http-equiv=refresh content=0;url=mpi/index.html>" >> target/doc/index.html
 ghp-import -n target/doc
-git remote -v
-git push -v origin gh-pages
+git push -f origin gh-pages
