114c2093f8eb42df2e3e24e599d2eb994e8bceb0
Benedikt Steinbusch <ben@rockshrub.de>
2015-07-23 15:57:40 +0200

    Try git credential helper.

--- ci/save-credentials.sh
+++ ci/save-credentials.sh
@@ -5,10 +5,9 @@ set -e
 TOKEN=${GH_TOKEN:?'GH_TOKEN not set!'}
 REPO=${TRAVIS_REPO_SLUG:?'TRAVIS_REPO_SLUG not set!'}
 
-echo "Writing OAuth token to .netrc."
-touch ${HOME}/.netrc
-chmod 0600 ${HOME}/.netrc
-echo "machine github.com login $TOKEN password x-oauth-basic" >> ${HOME}/.netrc
+echo "Writing OAuth token to git credentials"
+git config credential.helper store
+echo "https://${TOKEN}:@github.com" >> ${HOME}/.git-credentials
 
 echo "Setting origin to use HTTPS for pushing"
 git remote set-url --push origin "https://github.com/${REPO}"
