dfa64839506a925579cb3f2e5a0e65edb8de1b3f
Benedikt Steinbusch <ben@rockshrub.de>
2015-07-23 16:16:01 +0200

    Do some more CI debugging.

--- ci/deploy-docs.sh
+++ ci/deploy-docs.sh
@@ -6,4 +6,6 @@ travis-cargo doc
 
 # Manual doc deployment
 echo "<meta http-equiv=refresh content=0;url=mpi/index.html>" >> target/doc/index.html
-ghp-import -n -p target/doc
+ghp-import -n target/doc
+git remote -v
+git push -v origin gh-pages
--- ci/save-credentials.sh
+++ ci/save-credentials.sh
@@ -10,4 +10,4 @@ git config credential.helper store
 echo "https://${TOKEN}:@github.com" >> ${HOME}/.git-credentials
 
 echo "Setting origin to use HTTPS for pushing"
-git remote set-url --push origin "https://github.com/${REPO}"
+git remote set-url origin "https://github.com/${REPO}"
