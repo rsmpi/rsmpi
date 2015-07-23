df1c584a33aaf3428d466e39cc3ab9d5835edcb8
Benedikt Steinbusch <ben@rockshrub.de>
2015-07-23 11:52:44 +0200

    Debug Travis doc deployment woes.

--- ci/save-credentials.sh
+++ ci/save-credentials.sh
@@ -2,6 +2,18 @@
 
 set -e
 
+TOKEN=${GH_TOKEN:?'GH_TOKEN not set!'}
+
 touch ${HOME}/.netrc
 chmod 0600 ${HOME}/.netrc
-echo "machine github.com login ${GH_TOKEN:?'GH_TOKEN not set!'} password x-oauth-basic" >> ${HOME}/.netrc
+echo "Writing OAuth token to .netrc."
+echo "machine github.com login $TOKEN password x-oauth-basic" >> ${HOME}/.netrc
+
+echo "HOME looks like:"
+ls -la ${HOME}
+
+echo ".netrc contains:"
+grep -o -E "^machine[[:space:]]+[^[:space:]]+[[:space:]]+login" ~/.netrc 
+
+echo "trying clone:"
+git clone --verbose https://github.com/davisp/ghp-import
