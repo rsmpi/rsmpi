86831a35aab163c1faba5a3bc134ef4265c97ece
Benedikt Steinbusch <ben@rockshrub.de>
2015-09-26 15:27:17 +0200

    Add assertions to barrier example.

--- examples/barrier.rs
+++ examples/barrier.rs
@@ -6,7 +6,24 @@ fn main() {
     let universe = mpi::initialize().unwrap();
     let world = universe.world();
 
-    println!("Before barrier, rank {}.", world.rank());
-    world.barrier();
-    println!("After barrier, rank {}.", world.rank());
+    if world.rank() > 0 {
+        let msg = 1u64;
+        world.process_at_rank(0).send(&msg);
+        world.barrier();
+        let msg = 2u64;
+        world.process_at_rank(0).send(&msg);
+    } else {
+        let n = (world.size() - 1) as usize;
+        let mut buf = vec![0u64; 2 * n];
+        for x in buf[0..n].iter_mut() {
+            world.receive_into(x);
+        }
+        world.barrier();
+        for x in buf[n..2 * n].iter_mut() {
+            world.receive_into(x);
+        }
+        println!("{:?}", buf);
+        assert!(buf[0..n].iter().all(|&x| { x == 1 }));
+        assert!(buf[n..2 * n].iter().all(|&x| { x == 2 }));
+    }
 }
