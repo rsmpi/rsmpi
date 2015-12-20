ddf2d32fe16032a103375e0272fb715706b2b051
Benedikt Steinbusch <ben@rockshrub.de>
2015-12-21 00:26:32 +0100

    Have CI test the example in README.md.

---
+++ examples/readme.rs
@@ -0,0 +1,39 @@
+extern crate mpi;
+
+use mpi::traits::*;
+use mpi::request::WaitGuard;
+
+fn main() {
+    let universe = mpi::initialize().unwrap();
+    let world = universe.world();
+    let size = world.size();
+    let rank = world.rank();
+
+    let next_rank = if rank + 1 < size { rank + 1 } else { 0 };
+    let previous_rank = if rank - 1 >= 0 { rank - 1 } else { size - 1 };
+
+    let msg = vec![rank , 2 * rank, 4 * rank];
+    let _sreq = WaitGuard::from(world.process_at_rank(next_rank).immediate_send(&msg[..]));
+
+    let (msg, status) = world.receive_vec();
+    let msg = msg.unwrap();
+
+    println!("Process {} got message {:?}.\nStatus is: {:?}", rank, msg, status);
+    let x = status.source_rank();
+    assert_eq!(x, previous_rank);
+    assert_eq!(vec![x, 2 * x, 4 * x], msg);
+
+    let root_rank = 0;
+    let root_process = world.process_at_rank(root_rank);
+
+    let mut a;
+    if world.rank() == root_rank {
+        a = vec![2, 4, 8, 16];
+        println!("Root broadcasting value: {:?}.", &a[..]);
+    } else {
+        a = vec![0; 4];
+    }
+    root_process.broadcast_into(&mut a[..]);
+    println!("Rank {} received value: {:?}.", world.rank(), &a[..]);
+    assert_eq!(&a[..], &[2, 4, 8, 16]);
+}
