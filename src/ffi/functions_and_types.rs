a67820ee9d14f0f02d058127df9bb53bccca294d
Benedikt Steinbusch <ben@rockshrub.de>
2017-02-13 23:24:29 +0100

    chore: switch to servo bindgen

--- src/ffi/functions_and_types.rs
+++ src/ffi/functions_and_types.rs
@@ -1,3 +1,7 @@
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
 #![allow(missing_copy_implementations)]
+#![cfg_attr(test, allow(trivial_casts))]
 #![cfg_attr(feature="clippy", allow(used_underscore_binding))]
 include!(concat!(env!("OUT_DIR"), "/functions_and_types.rs"));
