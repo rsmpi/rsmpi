1b9f4539e01d4849889baba39a162fad498cd5f1
Andrew Gaspar <andrew.gaspar@outlook.com>
2018-10-08 16:49:43 -0600

    Add clarifying message to `Datatype` documentation

--- src/datatype.rs
+++ src/datatype.rs
@@ -661,6 +661,9 @@ impl<'a> From<&'a UncommittedUserDatatype> for UncommittedDatatypeRef<'a> {
 }
 
 /// A Datatype describes the layout of messages in memory.
+///
+/// `Datatype` always represents a committed datatype that can be immediately used for sending and
+/// receiving messages. `UncommittedDatatype` is used for datatypes that are possibly uncommitted.
 pub trait Datatype: AsRaw<Raw = MPI_Datatype> {}
 impl<'a, D> Datatype for &'a D where D: 'a + Datatype {}
 
