71ee259177d439908825e5136b2dde11c39680aa
Benedikt Steinbusch <ben@rockshrub.de>
2019-04-21 21:46:45 +0200

    remove work-around for bindgen issue #511

--- mpi-sys/src/rsmpi.h
+++ mpi-sys/src/rsmpi.h
@@ -40,11 +40,8 @@ extern const MPI_Message RSMPI_MESSAGE_NO_PROC;
 
 extern const MPI_Request RSMPI_REQUEST_NULL;
 
-// FIXME: bindgen issue #511
-// extern MPI_Status* const RSMPI_STATUS_IGNORE;
-extern MPI_Status* RSMPI_STATUS_IGNORE;
-// extern MPI_Status* const RSMPI_STATUSES_IGNORE;
-extern MPI_Status* RSMPI_STATUSES_IGNORE;
+extern MPI_Status* const RSMPI_STATUS_IGNORE;
+extern MPI_Status* const RSMPI_STATUSES_IGNORE;
 
 extern const int RSMPI_IDENT;
 extern const int RSMPI_CONGRUENT;
