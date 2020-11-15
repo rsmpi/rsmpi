2457fd17b3f4c5860874fe16068fcb4589854849
Andrew Gaspar <andrew.gaspar@outlook.com>
2020-11-15 16:28:13 -0700

    Remove `.map_or` to keep MSRV at 1.36.0 on Unix and 1.40.0 on Windows (bindgen-> clang-sys ->libloading requires 1.40.0)

--- build.rs
+++ build.rs
@@ -1,5 +1,8 @@
 fn main() {
-    let is_msmpi = build_probe_mpi::probe().map_or(false, |lib| lib.version == "MS-MPI");
+    let is_msmpi = match build_probe_mpi::probe() {
+        Ok(lib) => lib.version == "MS-MPI",
+        _ => false,
+    };
 
     if is_msmpi {
         println!("cargo:rustc-cfg=msmpi");
