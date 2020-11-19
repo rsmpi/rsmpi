8525eeefbd600c2dda3b8be64cd73d77b68b5cb6
Andrew Gaspar <andrew.gaspar@outlook.com>
2020-11-18 17:23:13 -0700

    Remove outdated comment

--- build.rs
+++ build.rs
@@ -1,7 +1,6 @@
 use rustc_version::Version as RustcVersion;
 
 fn main() {
-    // Access to extern statics has to be marked unsafe after 1.13.0
     if rustc_version::version().unwrap() >= RustcVersion::parse("1.46.0").unwrap() {
         println!("cargo:rustc-cfg=track_caller_supported");
     }
