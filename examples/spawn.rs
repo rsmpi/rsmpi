1ec3a042952374b51e61611502e5566ee55233b9
Jed Brown <jed@jedbrown.org>
2022-12-04 07:33:15 -0700

    Implement MPI_Comm_spawn and MPI_Comm_spawn_multiple (fix #87)
    These were refactored from an earlier approach to use
    std::process::Command because existing tools can build Commands. They
    currently do not support environment variables, which can be implemented
    using MPI_Info.
    
    Based-on-patch-by: Andrew Gaspar

---
+++ examples/spawn.rs
@@ -0,0 +1,41 @@
+use std::env;
+use std::process::Command;
+
+use mpi::traits::*;
+
+fn main() -> Result<(), mpi::MpiError> {
+    let universe = mpi::initialize().unwrap();
+    let world = universe.world();
+
+    if let Some(parent) = world.parent() {
+        assert_eq!("from_parent", env::args().skip(1).next().unwrap());
+
+        parent.process_at_rank(0).send(&7i32);
+
+        println!(
+            "[{}/{}] Child universe {:?}",
+            world.rank(),
+            world.size(),
+            universe.size()
+        );
+    } else {
+        let child_size = 1;
+        let mut exe = Command::new(env::current_exe().unwrap());
+        exe.arg("from_parent");
+
+        let child = world.process_at_rank(0).spawn(&exe, child_size)?;
+
+        assert_eq!(child_size, child.remote_size());
+
+        if world.rank() == 0 {
+            assert_eq!(7i32, child.process_at_rank(0).receive().0);
+        }
+        println!(
+            "[{}/{}] Parent universe {:?}",
+            world.rank(),
+            world.size(),
+            universe.size(),
+        );
+    };
+    Ok(())
+}
