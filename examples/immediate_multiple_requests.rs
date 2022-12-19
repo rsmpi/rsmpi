rsmpi/rsmpi

commit 8f4b0bf1ac3069b9bceca5b448b63cc17e035d4e

Multiple Request Completion
* Attach data references to Request object
* Add RequestCollection object for interfacing with MPI wait_*() and test_*() functions
* Add three new examples/tests for this functionality

examples/immediate_multiple_requests.rs
@@ -1,0 +1,152 @@
+//! Example using request handlers.
+use mpi;
+use mpi::point_to_point::Status;
+use mpi::traits::*;
+
+const COUNT: usize = 256;
+
+fn main() {
+    let universe = mpi::initialize().unwrap();
+    let world = universe.world();
+    let size = world.size();
+    let rank = world.rank();
+
+    let x: i32 = 100;
+    let mut y: i32 = 0;
+
+    mpi::request::scope(|scope| {
+        let sreq = world.this_process().immediate_send(scope, &x);
+        let rreq = world.any_process().immediate_receive_into(scope, &mut y);
+        let result = rreq.wait_for_data();
+        assert_eq!(*result, x);
+        sreq.wait();
+    });
+
+    // Test wait_any()
+    let mut result: Vec<i32> = vec![0; COUNT];
+    mpi::request::multiple_scope(2 * COUNT, |scope, coll| {
+        let prev_proc = if rank > 0 { rank - 1 } else { size - 1 };
+        let next_proc = (rank + 1) % size;
+        for _ in 0..result.len() {
+            let sreq = world
+                .process_at_rank(next_proc)
+                .immediate_send(scope, &rank);
+            coll.add(sreq);
+        }
+        for val in result.iter_mut() {
+            let rreq = world
+                .process_at_rank(prev_proc)
+                .immediate_receive_into(scope, val);
+            coll.add(rreq);
+        }
+        let mut send_count = 0;
+        let mut recv_count = 0;
+        while coll.incomplete() > 0 {
+            let (_, _, result) = coll.wait_any().unwrap();
+            if *result == rank {
+                send_count += 1;
+            } else {
+                recv_count += 1;
+            }
+        }
+        assert_eq!(send_count, COUNT);
+        assert_eq!(recv_count, COUNT);
+    });
+
+    let mut result: Vec<i32> = vec![0; COUNT];
+    // Test wait_some()
+    mpi::request::multiple_scope(2 * COUNT, |scope, coll| {
+        let prev_proc = if rank > 0 { rank - 1 } else { size - 1 };
+        let next_proc = (rank + 1) % size;
+        for _ in 0..result.len() {
+            let sreq = world
+                .process_at_rank(next_proc)
+                .immediate_send(scope, &rank);
+            coll.add(sreq);
+        }
+        for val in result.iter_mut() {
+            let rreq = world
+                .process_at_rank(prev_proc)
+                .immediate_receive_into(scope, val);
+            coll.add(rreq);
+        }
+        let mut send_count = 0;
+        let mut recv_count = 0;
+        let mut some_buf: Vec<Option<(usize, Status, &i32)>> = vec![None; 2 * COUNT];
+        while coll.incomplete() > 0 {
+            let count = coll.wait_some(&mut some_buf);
+            println!("wait_some(): {} request(s) completed", count);
+            assert!(some_buf.iter().any(|elm| elm.is_some()));
+            for elm in some_buf.iter() {
+                if let Some((_, _, result)) = elm {
+                    if **result == rank {
+                        send_count += 1;
+                    } else {
+                        recv_count += 1;
+                    }
+                }
+            }
+        }
+        assert_eq!(send_count, COUNT);
+        assert_eq!(recv_count, COUNT);
+    });
+
+    let mut result: Vec<i32> = vec![0; COUNT];
+    // Test wait_all()
+    mpi::request::multiple_scope(2 * COUNT, |scope, coll| {
+        let prev_proc = if rank > 0 { rank - 1 } else { size - 1 };
+        let next_proc = (rank + 1) % size;
+        for _ in 0..result.len() {
+            let sreq = world
+                .process_at_rank(next_proc)
+                .immediate_send(scope, &rank);
+            coll.add(sreq);
+        }
+        for val in result.iter_mut() {
+            let rreq = world
+                .process_at_rank(prev_proc)
+                .immediate_receive_into(scope, val);
+            coll.add(rreq);
+        }
+        let mut out: Vec<Option<(usize, Status, &i32)>> = vec![None; 2 * COUNT];
+        coll.wait_all(&mut out);
+        let mut send_count = 0;
+        let mut recv_count = 0;
+        for elm in out {
+            let (_, _, result) = elm.unwrap();
+            if *result == rank {
+                send_count += 1;
+            } else {
+                recv_count += 1;
+            }
+        }
+        assert_eq!(send_count, COUNT);
+        assert_eq!(recv_count, COUNT);
+    });
+
+    // Check wait_*() with a buffer of increasing values
+    let x: Vec<i32> = (0..COUNT as i32).collect();
+    let mut result: Vec<i32> = vec![0; COUNT];
+    mpi::request::multiple_scope(COUNT, |scope, coll| {
+        let prev_proc = if rank > 0 { rank - 1 } else { size - 1 };
+        let next_proc = (rank + 1) % size;
+        for elm in &x {
+            let sreq = world
+                .process_at_rank(next_proc)
+                .immediate_send(scope, elm);
+            coll.add(sreq);
+        }
+        for val in result.iter_mut() {
+            let rreq = world
+                .process_at_rank(prev_proc)
+                .immediate_receive_into(scope, val);
+            coll.add(rreq);
+        }
+        let mut out: Vec<Option<(usize, Status, &i32)>> = vec![None; 2 * COUNT];
+        coll.wait_all(&mut out);
+        assert!(out.iter().all(|elm| elm.is_some()));
+    });
+    // Ensure the result and x are an incrementing array of integers
+    result.sort();
+    assert_eq!(result, x);
+}

examples/immediate.rs
@@ -41,14 +41,14 @@ fn main() {
 
     y = 0.0;
     mpi::request::scope(|scope| {
-        let _sreq: WaitGuard<_> = world
+        let _sreq: WaitGuard<_, _> = world
             .this_process()
             .immediate_synchronous_send(scope, &x)
             .into();
         let preq = world.any_process().immediate_matched_probe();
         assert!(preq.is_some());
         let (msg, _) = preq.unwrap();
-        let _rreq: WaitGuard<_> = msg.immediate_matched_receive_into(scope, &mut y).into();
+        let _rreq: WaitGuard<_, _> = msg.immediate_matched_receive_into(scope, &mut y).into();
     });
     assert_eq!(x, y);
 

examples/immediate_barrier_multiple.rs
@@ -1,0 +1,112 @@
+/// Example using different types of requests with a request collection.
+use mpi;
+use mpi::traits::*;
+
+#[cfg(msmpi)]
+fn main() {
+}
+
+const COUNT: usize = 128;
+
+#[cfg(not(msmpi))]
+fn main() {
+    let universe = mpi::initialize().unwrap();
+    let world = universe.world();
+
+    // Try wait_any()
+    mpi::request::multiple_scope(COUNT, |_, coll| {
+        for _ in 0..COUNT {
+            let req = world.immediate_barrier();
+            coll.add(req);
+        }
+        while coll.incomplete() > 0 {
+            coll.wait_any().unwrap();
+        }
+    });
+
+    // Try wait_some()
+    mpi::request::multiple_scope(COUNT, |_, coll| {
+        for _ in 0..COUNT {
+            let req = world.immediate_barrier();
+            coll.add(req);
+        }
+        let mut result = vec![None; COUNT];
+        while coll.incomplete() > 0 {
+            let count = coll.wait_some(&mut result);
+            let mut i = 0;
+            for elm in result.iter() {
+                if let Some(_) = elm {
+                    i += 1;
+                }
+            }
+            assert_eq!(i, count);
+        }
+    });
+
+    // Try wait_all()
+    mpi::request::multiple_scope(COUNT, |_, coll| {
+        for _ in 0..COUNT {
+            let req = world.immediate_barrier();
+            coll.add(req);
+        }
+        let mut result = vec![None; COUNT ];
+        coll.wait_all(&mut result);
+        assert!(result.iter().all(|elm| elm.is_some()));
+    });
+
+    // Try test_any()
+    mpi::request::multiple_scope(COUNT, |_, coll| {
+        for _ in 0..COUNT {
+            let req = world.immediate_barrier();
+            coll.add(req);
+        }
+        let mut finished = 0;
+        while coll.incomplete() > 0 {
+            if coll.test_any().is_some() {
+                finished += 1;
+            }
+            assert_eq!(coll.incomplete(), COUNT - finished);
+        }
+        assert_eq!(finished, COUNT);
+    });
+
+    // Try test_some()
+    mpi::request::multiple_scope(COUNT, |_, coll| {
+        for _ in 0..COUNT {
+            let req = world.immediate_barrier();
+            coll.add(req);
+        }
+        let mut result = vec![None; COUNT];
+        let mut finished = 0;
+        while coll.incomplete() > 0 {
+            let count = coll.test_some(&mut result);
+            finished += count;
+            if count > 0 {
+                let mut i = 0;
+                for elm in result.iter() {
+                    if let Some(_) = elm {
+                        i += 1;
+                    }
+                }
+                assert_eq!(i, count);
+            }
+            assert_eq!(coll.incomplete(), COUNT - finished);
+        }
+        assert_eq!(finished, COUNT);
+    });
+
+    // Try test_all()
+    mpi::request::multiple_scope(COUNT, |_, coll| {
+        for _ in 0..COUNT {
+            let req = world.immediate_barrier();
+            coll.add(req);
+        }
+        let mut result = vec![None; COUNT];
+        while coll.incomplete() > 0 {
+            if coll.test_all(&mut result) {
+                assert_eq!(coll.incomplete(), 0);
+            }
+        }
+        assert!(result.iter().all(|elm| elm.is_some()));
+    });
+}

examples/immediate_multiple_test.rs
@@ -1,0 +1,97 @@
+/// Example showing usage of test_any(), test_some() and test_all().
+use mpi;
+use mpi::Rank;
+use mpi::traits::*;
+use mpi::request::{Scope, RequestCollection};
+
+const COUNT: usize = 128;
+
+/// Send and receive COUNT number of immediate requests.
+fn send_recv<'a, C: Communicator, S: Scope<'a> + Copy>(
+    world: C,
+    scope: S,
+    coll: &mut RequestCollection<'a, [i32; 4]>,
+    next_proc: Rank,
+    prev_proc: Rank,
+    x: &'a [[i32; 4]],
+    recv: &'a mut [[i32; 4]],
+) {
+    for elm in x {
+        let sreq = world
+            .process_at_rank(next_proc)
+            .immediate_send(scope, elm);
+        coll.add(sreq);
+    }
+    for elm in recv.iter_mut() {
+        let rreq = world
+            .process_at_rank(prev_proc)
+            .immediate_receive_into(scope, elm);
+        coll.add(rreq);
+    }
+}
+
+/// Ensure that the result buffer, containing the data for both send and receive
+/// requests, matches what was sent in the buffer x.
+fn check_result_buffer(x: &[[i32; 4]], mut result_buf: Vec<[i32; 4]>) {
+    result_buf.sort();
+    assert_eq!(result_buf.len(), 2 * x.len());
+    for (i, x_val) in x.iter().enumerate() {
+        // The result buffer has both send values and receive values
+        assert_eq!(result_buf[2 * i], *x_val);
+        assert_eq!(result_buf[2 * i + 1], *x_val);
+    }
+}
+
+fn main() {
+    let universe = mpi::initialize().unwrap();
+    let world = universe.world();
+    let rank = world.rank();
+    let size = world.size();
+
+    let next_proc = (rank + 1) % size;
+    let prev_proc = if rank > 0 { rank - 1 } else { size - 1 };
+
+    let x: Vec<[i32; 4]> = (0..COUNT as i32).map(|i| [i, i + 1, i + 2, i + 3]).collect();
+    let mut recv: Vec<[i32; 4]> = vec![[0, 0, 0, 0]; COUNT];
+    mpi::request::multiple_scope(COUNT, |scope, coll| {
+        send_recv(world, scope, coll, next_proc, prev_proc, &x, &mut recv);
+
+        let mut buf = vec![];
+        while coll.incomplete() > 0 {
+            if let Some((_, _, data)) = coll.test_any() {
+                buf.push(*data);
+            }
+        }
+        check_result_buffer(&x, buf);
+    });
+
+    let mut recv: Vec<[i32; 4]> = vec![[0, 0, 0, 0]; COUNT];
+    mpi::request::multiple_scope(COUNT, |scope, coll| {
+        send_recv(world, scope, coll, next_proc, prev_proc, &x, &mut recv);
+
+        let mut complete = vec![None; 2 * COUNT];
+        let mut buf = vec![];
+        while coll.incomplete() > 0 {
+            let count = coll.test_some(&mut complete);
+            if count > 0 {
+                println!("test_some(): {} request(s) completed", count);
+                for elm in complete.iter() {
+                    if let Some((_, _, data)) = elm {
+                        buf.push(**data);
+                    }
+                }
+            }
+        }
+        check_result_buffer(&x, buf);
+    });
+
+    let mut recv: Vec<[i32; 4]> = vec![[0, 0, 0, 0]; COUNT];
+    mpi::request::multiple_scope(COUNT, |scope, coll| {
+        send_recv(world, scope, coll, next_proc, prev_proc, &x, &mut recv);
+
+        let mut complete = vec![None; 2 * COUNT];
+        while !coll.test_all(&mut complete) { }
+        let buf: Vec<[i32; 4]> = complete.iter().map(|elm| *elm.unwrap().2).collect();
+        check_result_buffer(&x, buf);
+    });
+}

src/collective.rs
@@ -310,10 +310,11 @@ pub trait CommunicatorCollectives: Communicator {
     /// # Standard section(s)
     ///
     /// 5.12.1
-    fn immediate_barrier(&self) -> Request<'static> {
+    fn immediate_barrier(&self) -> Request<'static, ()> {
         unsafe {
             Request::from_raw(
                 with_uninitialized(|request| ffi::MPI_Ibarrier(self.as_raw(), request)).1,
+                &(),
                 StaticScope,
             )
         }
@@ -329,12 +330,12 @@ pub trait CommunicatorCollectives: Communicator {
     /// # Standard section(s)
     ///
     /// 5.12.5
-    fn immediate_all_gather_into<'a, Sc, S: ?Sized, R: ?Sized>(
+    fn immediate_all_gather_into<'a, S: ?Sized, R: ?Sized, Sc>(
         &self,
         scope: Sc,
         sendbuf: &'a S,
         recvbuf: &'a mut R,
-    ) -> Request<'a, Sc>
+    ) -> Request<'a, R, Sc>
     where
         S: 'a + Buffer,
         R: 'a + BufferMut,
@@ -356,6 +357,7 @@ pub trait CommunicatorCollectives: Communicator {
                     )
                 })
                 .1,
+                recvbuf,
                 scope,
             )
         }
@@ -371,12 +373,12 @@ pub trait CommunicatorCollectives: Communicator {
     /// # Standard section(s)
     ///
     /// 5.12.5
-    fn immediate_all_gather_varcount_into<'a, Sc, S: ?Sized, R: ?Sized>(
+    fn immediate_all_gather_varcount_into<'a, S: ?Sized, R: ?Sized, Sc>(
         &self,
         scope: Sc,
         sendbuf: &'a S,
         recvbuf: &'a mut R,
-    ) -> Request<'a, Sc>
+    ) -> Request<'a, R, Sc>
     where
         S: 'a + Buffer,
         R: 'a + PartitionedBufferMut,
@@ -398,6 +400,7 @@ pub trait CommunicatorCollectives: Communicator {
                     )
                 })
                 .1,
+                recvbuf,
                 scope,
             )
         }
@@ -412,12 +415,12 @@ pub trait CommunicatorCollectives: Communicator {
     /// # Standard section(s)
     ///
     /// 5.12.6
-    fn immediate_all_to_all_into<'a, Sc, S: ?Sized, R: ?Sized>(
+    fn immediate_all_to_all_into<'a, S: ?Sized, R: ?Sized, Sc>(
         &self,
         scope: Sc,
         sendbuf: &'a S,
         recvbuf: &'a mut R,
-    ) -> Request<'a, Sc>
+    ) -> Request<'a, R, Sc>
     where
         S: 'a + Buffer,
         R: 'a + BufferMut,
@@ -439,6 +442,7 @@ pub trait CommunicatorCollectives: Communicator {
                     )
                 })
                 .1,
+                recvbuf,
                 scope,
             )
         }
@@ -449,12 +453,12 @@ pub trait CommunicatorCollectives: Communicator {
     /// # Standard section(s)
     ///
     /// 5.12.6
-    fn immediate_all_to_all_varcount_into<'a, Sc, S: ?Sized, R: ?Sized>(
+    fn immediate_all_to_all_varcount_into<'a, S: ?Sized, R: ?Sized, Sc>(
         &self,
         scope: Sc,
         sendbuf: &'a S,
         recvbuf: &'a mut R,
-    ) -> Request<'a, Sc>
+    ) -> Request<'a, R, Sc>
     where
         S: 'a + PartitionedBuffer,
         R: 'a + PartitionedBufferMut,
@@ -477,6 +481,7 @@ pub trait CommunicatorCollectives: Communicator {
                     )
                 })
                 .1,
+                recvbuf,
                 scope,
             )
         }
@@ -492,13 +497,13 @@ pub trait CommunicatorCollectives: Communicator {
     /// # Standard section(s)
     ///
     /// 5.12.8
-    fn immediate_all_reduce_into<'a, Sc, S: ?Sized, R: ?Sized, O>(
+    fn immediate_all_reduce_into<'a, S: ?Sized, R: ?Sized, O, Sc>(
         &self,
         scope: Sc,
         sendbuf: &'a S,
         recvbuf: &'a mut R,
         op: O,
-    ) -> Request<'a, Sc>
+    ) -> Request<'a, R, Sc>
     where
         S: 'a + Buffer,
         R: 'a + BufferMut,
@@ -519,6 +524,7 @@ pub trait CommunicatorCollectives: Communicator {
                     )
                 })
                 .1,
+                recvbuf,
                 scope,
             )
         }
@@ -535,13 +541,13 @@ pub trait CommunicatorCollectives: Communicator {
     /// # Standard section(s)
     ///
     /// 5.12.9
-    fn immediate_reduce_scatter_block_into<'a, Sc, S: ?Sized, R: ?Sized, O>(
+    fn immediate_reduce_scatter_block_into<'a, S: ?Sized, R: ?Sized, O, Sc>(
         &self,
         scope: Sc,
         sendbuf: &'a S,
         recvbuf: &'a mut R,
         op: O,
-    ) -> Request<'a, Sc>
+    ) -> Request<'a, R, Sc>
     where
         S: 'a + Buffer,
         R: 'a + BufferMut,
@@ -563,6 +569,7 @@ pub trait CommunicatorCollectives: Communicator {
                     )
                 })
                 .1,
+                recvbuf,
                 scope,
             )
         }
@@ -578,13 +585,13 @@ pub trait CommunicatorCollectives: Communicator {
     /// # Standard section(s)
     ///
     /// 5.12.11
-    fn immediate_scan_into<'a, Sc, S: ?Sized, R: ?Sized, O>(
+    fn immediate_scan_into<'a, S: ?Sized, R: ?Sized, O, Sc>(
         &self,
         scope: Sc,
         sendbuf: &'a S,
         recvbuf: &'a mut R,
         op: O,
-    ) -> Request<'a, Sc>
+    ) -> Request<'a, R, Sc>
     where
         S: 'a + Buffer,
         R: 'a + BufferMut,
@@ -605,6 +612,7 @@ pub trait CommunicatorCollectives: Communicator {
                     )
                 })
                 .1,
+                recvbuf,
                 scope,
             )
         }
@@ -620,13 +628,13 @@ pub trait CommunicatorCollectives: Communicator {
     /// # Standard section(s)
     ///
     /// 5.12.12
-    fn immediate_exclusive_scan_into<'a, Sc, S: ?Sized, R: ?Sized, O>(
+    fn immediate_exclusive_scan_into<'a, S: ?Sized, R: ?Sized, O, Sc>(
         &self,
         scope: Sc,
         sendbuf: &'a S,
         recvbuf: &'a mut R,
         op: O,
-    ) -> Request<'a, Sc>
+    ) -> Request<'a, R, Sc>
     where
         S: 'a + Buffer,
         R: 'a + BufferMut,
@@ -647,6 +655,7 @@ pub trait CommunicatorCollectives: Communicator {
                     )
                 })
                 .1,
+                recvbuf,
                 scope,
             )
         }
@@ -1056,11 +1065,11 @@ pub trait Root: AsCommunicator {
     /// # Standard section(s)
     ///
     /// 5.12.2
-    fn immediate_broadcast_into<'a, Sc, Buf: ?Sized>(
+    fn immediate_broadcast_into<'a, Buf: ?Sized, Sc>(
         &self,
         scope: Sc,
         buf: &'a mut Buf,
-    ) -> Request<'a, Sc>
+    ) -> Request<'a, Buf, Sc>
     where
         Buf: 'a + BufferMut,
         Sc: Scope<'a>,
@@ -1078,6 +1087,7 @@ pub trait Root: AsCommunicator {
                     )
                 })
                 .1,
+                buf,
                 scope,
             )
         }
@@ -1094,7 +1104,11 @@ pub trait Root: AsCommunicator {
     /// # Standard section(s)
     ///
     /// 5.12.3
-    fn immediate_gather_into<'a, Sc, S: ?Sized>(&self, scope: Sc, sendbuf: &'a S) -> Request<'a, Sc>
+    fn immediate_gather_into<'a, S: ?Sized, Sc>(
+        &self,
+        scope: Sc,
+        sendbuf: &'a S,
+    ) -> Request<'a, S, Sc>
     where
         S: 'a + Buffer,
         Sc: Scope<'a>,
@@ -1116,6 +1130,7 @@ pub trait Root: AsCommunicator {
                     )
                 })
                 .1,
+                sendbuf,
                 scope,
             )
         }
@@ -1132,12 +1147,12 @@ pub trait Root: AsCommunicator {
     /// # Standard section(s)
     ///
     /// 5.12.3
-    fn immediate_gather_into_root<'a, Sc, S: ?Sized, R: ?Sized>(
+    fn immediate_gather_into_root<'a, S: ?Sized, R: ?Sized, Sc>(
         &self,
         scope: Sc,
         sendbuf: &'a S,
         recvbuf: &'a mut R,
-    ) -> Request<'a, Sc>
+    ) -> Request<'a, R, Sc>
     where
         S: 'a + Buffer,
         R: 'a + BufferMut,
@@ -1161,6 +1176,7 @@ pub trait Root: AsCommunicator {
                     )
                 })
                 .1,
+                recvbuf,
                 scope,
             )
         }
@@ -1181,7 +1197,7 @@ pub trait Root: AsCommunicator {
         &self,
         scope: Sc,
         sendbuf: &'a S,
-    ) -> Request<'a, Sc>
+    ) -> Request<'a, S, Sc>
     where
         S: 'a + Buffer,
         Sc: Scope<'a>,
@@ -1204,6 +1220,7 @@ pub trait Root: AsCommunicator {
                     )
                 })
                 .1,
+                sendbuf,
                 scope,
             )
         }
@@ -1225,7 +1242,7 @@ pub trait Root: AsCommunicator {
         scope: Sc,
         sendbuf: &'a S,
         recvbuf: &'a mut R,
-    ) -> Request<'a, Sc>
+    ) -> Request<'a, R, Sc>
     where
         S: 'a + Buffer,
         R: 'a + PartitionedBufferMut,
@@ -1249,6 +1266,7 @@ pub trait Root: AsCommunicator {
                     )
                 })
                 .1,
+                recvbuf,
                 scope,
             )
         }
@@ -1269,7 +1287,7 @@ pub trait Root: AsCommunicator {
         &self,
         scope: Sc,
         recvbuf: &'a mut R,
-    ) -> Request<'a, Sc>
+    ) -> Request<'a, R, Sc>
     where
         R: 'a + BufferMut,
         Sc: Scope<'a>,
@@ -1291,6 +1309,7 @@ pub trait Root: AsCommunicator {
                     )
                 })
                 .1,
+                recvbuf,
                 scope,
             )
         }
@@ -1312,7 +1331,7 @@ pub trait Root: AsCommunicator {
         scope: Sc,
         sendbuf: &'a S,
         recvbuf: &'a mut R,
-    ) -> Request<'a, Sc>
+    ) -> Request<'a, R, Sc>
     where
         S: 'a + Buffer,
         R: 'a + BufferMut,
@@ -1336,6 +1355,7 @@ pub trait Root: AsCommunicator {
                     )
                 })
                 .1,
+                recvbuf,
                 scope,
             )
         }
@@ -1356,7 +1376,7 @@ pub trait Root: AsCommunicator {
         &self,
         scope: Sc,
         recvbuf: &'a mut R,
-    ) -> Request<'a, Sc>
+    ) -> Request<'a, R, Sc>
     where
         R: 'a + BufferMut,
         Sc: Scope<'a>,
@@ -1379,6 +1399,7 @@ pub trait Root: AsCommunicator {
                     )
                 })
                 .1,
+                recvbuf,
                 scope,
             )
         }
@@ -1400,7 +1421,7 @@ pub trait Root: AsCommunicator {
         scope: Sc,
         sendbuf: &'a S,
         recvbuf: &'a mut R,
-    ) -> Request<'a, Sc>
+    ) -> Request<'a, R, Sc>
     where
         S: 'a + PartitionedBuffer,
         R: 'a + BufferMut,
@@ -1424,6 +1445,7 @@ pub trait Root: AsCommunicator {
                     )
                 })
                 .1,
+                recvbuf,
                 scope,
             )
         }
@@ -1446,7 +1468,7 @@ pub trait Root: AsCommunicator {
         scope: Sc,
         sendbuf: &'a S,
         op: O,
-    ) -> Request<'a, Sc>
+    ) -> Request<'a, S, Sc>
     where
         S: 'a + Buffer,
         O: 'a + Operation,
@@ -1468,6 +1490,7 @@ pub trait Root: AsCommunicator {
                     )
                 })
                 .1,
+                sendbuf,
                 scope,
             )
         }
@@ -1491,7 +1514,7 @@ pub trait Root: AsCommunicator {
         sendbuf: &'a S,
         recvbuf: &'a mut R,
         op: O,
-    ) -> Request<'a, Sc>
+    ) -> Request<'a, R, Sc>
     where
         S: 'a + Buffer,
         R: 'a + BufferMut,
@@ -1514,6 +1537,7 @@ pub trait Root: AsCommunicator {
                     )
                 })
                 .1,
+                recvbuf,
                 scope,
             )
         }

src/lib.rs
@@ -179,6 +179,8 @@ pub type Count = c_int;
 pub type Tag = c_int;
 /// An address in memory
 pub type Address = MPI_Aint;
+/// Rank of each process.
+pub type Rank = c_int;
 
 /// IntArray is used to translate Rust bool values to and from the int-bool types preferred by MPI
 /// without incurring allocation in the common case.

commit 827d6086cea13c3c042ffe13d1219c838d0ee269

examples/immediate_multiple_requests: fix race condition
When accepting a receive from any process, another process can complete
this phase and move on to sending to neighbors. Race introduced in
8f4b0bf1ac3069b9bceca5b448b63cc17e035d4e

examples/immediate_multiple_requests.rs
@@ -16,7 +16,7 @@ fn main() {
 
     mpi::request::scope(|scope| {
         let sreq = world.this_process().immediate_send(scope, &x);
-        let rreq = world.any_process().immediate_receive_into(scope, &mut y);
+        let rreq = world.this_process().immediate_receive_into(scope, &mut y);
         let result = rreq.wait_for_data();
         assert_eq!(*result, x);
         sreq.wait();