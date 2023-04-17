d478a7ba49965c5c415e49a092c10829f4bfc0c0
Keefe Huang <keefe.huang@tum.de>
2023-04-17 19:09:58 +0200

    Add complex feature
    Add feature "complex" and optional dendency "num_complex".
    Tie `Complex<T>` implementation with C types.

--- src/datatype.rs
+++ src/datatype.rs
@@ -213,6 +213,16 @@ equivalent_system_datatype!(usize, ffi::RSMPI_UINT64_T);
 #[cfg(target_pointer_width = "64")]
 equivalent_system_datatype!(isize, ffi::RSMPI_INT64_T);
 
+
+#[cfg(feature = "complex")]
+/// Implement direct equivalence for complex types
+pub mod complex_datatype {
+    use super::{ffi, DatatypeRef, SystemDatatype, Equivalence, FromRaw};
+    use num_complex::{Complex32, Complex64};
+    equivalent_system_datatype!(Complex32, ffi::RSMPI_FLOAT_COMPLEX);
+    equivalent_system_datatype!(Complex64, ffi::RSMPI_DOUBLE_COMPLEX);
+}
+
 /// A user defined MPI datatype
 ///
 /// # Standard section(s)
