913ad8ac7d05ef21b11e3bbf0b9b7982e7ad8ae7
Keefe Huang <30915130+keefehuang@users.noreply.github.com>
2023-10-19 16:40:29 +0200

    Implement Equivalence trait for complex types (#159)
    * Extern required complex types from mpi
    
    * Add complex feature
    
    Add feature "complex" and optional dendency "num_complex".
    Tie `Complex<T>` implementation with C types.
    
    * Add complex number example to example/
    
    * upgrade MSRV to 1.60 (#161)
    
    CI was failing because 1.60-incompatible dependencies were pulled in so
    may as well upgrade what we can.
    
    * Add RSMPI_UNWEIGHTED (#160)
    
    Add RSMPI_UNWEIGHTED() as a function because MPI_UNWEIGHTED can be a link-time constant, which can't be used to initialize an object with static storage duration.
    
    * docs: remove "Process management" as not implemented
    
    https://github.com/rsmpi/rsmpi/pull/140
    
    * Cargo upgrade and MSRV 1.65
    
    MSRV of transitive deps had already updated so this is just reflecting reality.
    
    * Merge conflict Cargo.toml
    
    * Config so that complex code runs only with feature
    
    * Tidy up dependencies
    
    Remove unneeded `derive` feature comments.
    Arranged in dependencies in alphabetical order.
    Remove unneeded spaces.
    
    * Remove #cfg complex and update Cargo.toml
    
    * Add complex feature flag to window runner
    
    * Fix complex_number test
    
    * cargo fmt
    
    ---------
    
    Co-authored-by: Keefe Huang <keefe.huang@tum.de>
    Co-authored-by: Jed Brown <jed@jedbrown.org>
    Co-authored-by: Matthew Scroggs <matthew.w.scroggs@gmail.com>

---
+++ examples/complex_numbers.rs
@@ -0,0 +1,32 @@
+extern crate mpi;
+use mpi::traits::*;
+use num_complex::Complex64;
+
+fn main() {
+    {
+        let universe = mpi::initialize().unwrap();
+        let world = universe.world();
+
+        let root_process = world.process_at_rank(0);
+
+        let mut data = if world.rank() == 0 {
+            vec![
+                Complex64::new(1.0, 2.0),
+                Complex64::new(2.0, 3.0),
+                Complex64::new(3.0, 4.0),
+            ]
+        } else {
+            vec![Complex64::new(0.0, 0.0); 3]
+        };
+
+        root_process.broadcast_into(&mut data);
+        assert_eq!(
+            vec![
+                Complex64::new(1.0, 2.0),
+                Complex64::new(2.0, 3.0),
+                Complex64::new(3.0, 4.0),
+            ],
+            data
+        );
+    }
+}
